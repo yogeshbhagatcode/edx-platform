5e7d05418389ae8e59d8048193f1c73b5ba77155
Tim McCormack <tmccormack@edx.org>
2023-10-23 14:51:31 +0000

    temp: Some working code for logging Waffle model changes

--- lms/djangoapps/certificates/config.py
+++ lms/djangoapps/certificates/config.py
@@ -43,3 +43,50 @@ SEND_CERTIFICATE_CREATED_SIGNAL = SettingToggle('SEND_CERTIFICATE_CREATED_SIGNAL
 # .. toggle_target_removal_date: 2024-01-01
 # .. toggle_tickets: TODO
 SEND_CERTIFICATE_REVOKED_SIGNAL = SettingToggle('SEND_CERTIFICATE_REVOKED_SIGNAL', default=False, module_name=__name__)
+
+
+from django.db.models import signals
+from django.dispatch import receiver
+
+import waffle.models
+
+def report_waffle_change(model_short_name, instance, created, fields):
+    verb = "created" if created else "updated"
+    state_desc = ", ".join(f"{field}={repr(getattr(instance, field))}" for field in fields)
+    print(f"📨📨📨📨📨 Waffle {model_short_name} {instance.name!r} was {verb}. New config: {state_desc}")
+
+def report_waffle_delete(model_short_name, instance):
+    print(f"📨📨📨📨📨 Waffle {model_short_name} {instance.name!r} was deleted")
+
+WAFFLE_MODELS_TO_OBSERVE = [
+    {
+        'model': waffle.models.Flag,
+        'short_name': 'flag',
+        'fields': ['everyone', 'percent', 'superusers', 'staff', 'authenticated', 'note', 'languages'],
+    },
+    {
+        'model': waffle.models.Switch,
+        'short_name': 'switch',
+        'fields': ['active', 'note'],
+    },
+    {
+        'model': waffle.models.Sample,
+        'short_name': 'sample',
+        'fields': ['percent', 'note'],
+    },
+]
+
+
+def register_waffle_observation(config):
+    @receiver(signals.post_save, sender=config['model'])
+    def log_waffle_change(*args, instance, created, **kwargs):
+        report_waffle_change(config['short_name'], instance, created, config['fields'])
+
+    @receiver(signals.post_delete, sender=config['model'])
+    def log_waffle_change(*args, instance, **kwargs):
+        report_waffle_delete(config['short_name'], instance)
+
+
+for config in WAFFLE_MODELS_TO_OBSERVE:
+    # Pass config to function to capture value properly.
+    register_waffle_observation(config)
